71f2c9354cba73b89786dbaf6372b5e9171ded42
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-07-07 17:37:23 +0800

    sm8250-common: Build libcodec2_hidl_shim

--- kona.mk
+++ kona.mk
@@ -260,6 +260,7 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
     libcodec2_soft_common.vendor \
     libsfplugin_ccodec_utils.vendor \
+    libcodec2_hidl_shim.vendor
 
 ifneq ($(TARGET_IS_TABLET),true)
 # NFC
