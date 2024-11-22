e7a7f64f297c4a7d85acadb6698cde6cd01843da
pachadomenic <pachadomenic2010@gmail.com>
2024-11-22 22:58:03 +0200

    sm8250-common: Update vendor to October SPL
    Change-Id: If01aca3fa451b62c316c75300e2fe1832ea56454

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -190,7 +190,7 @@ SOONG_CONFIG_XIAOMI_KONA_ROOTDIR_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-09-01
+VENDOR_SECURITY_PATCH := 2024-10-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
