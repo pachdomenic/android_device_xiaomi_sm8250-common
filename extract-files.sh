7a5241c7e21bebdc7f95e05b21af00c83a540d7e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-25 04:40:45 +0800

    sm8250-common: Allow gettid syscall for atfwd@2.0
    Change-Id: I0d7854b73e0b7e822edce89b9a3d6e33e1f45fc6

--- extract-files.sh
+++ extract-files.sh
@@ -80,11 +80,18 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            grep -q 'gettid: ' "${2}" || echo 'gettid: 1' >> "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         vendor/lib64/libril-qc-hal-qmi.so)
             [ "$2" = "" ] && return 0
             sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
             ;;
-            vendor/lib64/mediadrm/libwvdrmengine.so | vendor/lib64/libwvhidl.so)
+        vendor/lib64/mediadrm/libwvdrmengine.so | vendor/lib64/libwvhidl.so)
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
         *)
