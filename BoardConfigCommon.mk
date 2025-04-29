781f116bf2d788b546fcfe45ab9a10ef5ff67ff5
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:24:23 +0100

    sm8250-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ endif
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_kona
 TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_kona
+$(call soong_config_set,libinit,vendor_init_lib,//$(COMMON_PATH):init_xiaomi_kona)>>>>>>> 842be10 (sm8250-common: Set vendor init lib via soong config)
 
 # Kernel
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
