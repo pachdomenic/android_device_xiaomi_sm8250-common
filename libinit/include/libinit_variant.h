9fa23a3dcb81f1561561ec2ad9ff24caf48aff59
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-10-30 01:29:27 +0100

    sm8250-common: libinit: Use #pragma once
    Change-Id: I33db772ba211e7c124cd3d381e226e1331269218

--- libinit/include/libinit_variant.h
+++ libinit/include/libinit_variant.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef LIBINIT_VARIANT_H
-#define LIBINIT_VARIANT_H
+#pragma once
 
 #include <libvariant.h>
 
@@ -17,5 +16,3 @@ void search_variant(const std::vector<variant_info>& variants);
 
 
 void set_variant_props(const variant_info_t variant);
-
-#endif  // LIBINIT_VARIANT_H
